5eb7876c02b4bccb3e88e6ad22974ec2be4f2aee
Tiffany Mwangi <tiffany.mwagi@student.moringaschool.com>
2023-06-28 22:17:57 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,31 @@
+//function foo(){
+    //return 'bar';
+//}
+
+ //const foo = function(){
+    return 'bar';
+
+
+//const sum = 1 + 1
+
+//const add = (parameter1, parameter2) => parameter1 + parameter2;
+//add(2,3); 
+ //}
+
+ const foo = function(){
+  return 'bar';
+}
+
+const divide = () => {
+    return 2000 / 100;
+  };
+  
+  const square = (num) => {
+    return num * num;
+  };
+  
+  const add = (num1, num2) => {
+    return num1 + num2;
+  };
+  
+  
